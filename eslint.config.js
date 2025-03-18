b990e814681453ca075164f09e3154f9b6675fab
Mirte de Zeeuw <s153655@ap.be>
2025-03-18 14:59:51 +0100

    Configure PostCSS, Autoprefixer, and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  parserOptions: {
+    ecmaVersion: 2020, // Or a higher version depending on your project needs
+    sourceType: 'module', // Enables ES Modules (import/export) support
+  },
+  env: {
+    browser: true, // Or node: true if you're targeting Node.js
+  },
+  extends: ['eslint:recommended'], // You can add more extensions if needed
+};
